0f6825b53c2cca120de7ca2885e8dd4c4f682b53
Natasha Hivoyno <xixi.ru@mail.ru>
2016-01-20 18:51:22 +0300

    первый коммит

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp'),
+	concatCss = require('gulp-concat-css'),
+	minifyCSS = require('gulp-rename'),
+	notify = require('gulp-notify'),
+	rename = require('gulp-minify-css'),
+	browserSync = require('browser-sync');
+
+
+gulp.task('default', function () {
+  gulp.src('css/*.css')
+    .pipe(concatCss("bundle.css"))
+    .pipe(minifyCSS())
+    .pipe(rename("bundle.min.css"))
+    .pipe(notify("Done!"))
+    .pipe(gulp.dest('out/'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch([
+		'app/*.html',
+		'app/js/**/*.js',
+		'app/css/**/*.css'
+		]).on('change',browserSync.reload);
+});
+
+
+gulp.task('default', ['server','watch']);
+
+gulp.task('server', function(){
+	browserSync({
+		port:9000,
+		server:{
+			baseDir: 'app'
+		}
+	});
+});
+
